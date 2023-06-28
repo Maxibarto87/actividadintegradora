fa13617727c22478ca130824574a0fd0499def18
Maxibarto87 <barto_02_07_87@outlook.com>
2023-06-28 15:27:18 -0300

    se agrego carpeta js

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+let elH1 = document.querySelector('h1');
+elH1.innerHTML = 'Administración de stock';
+elH1.style.color = '#F4702A';
+elH1.style.fontSize = '60px';
+
+console.log(elH1.innerHTML);
+
+
+let navBar = document.querySelector('.nav-bar');
+navBar.onmouseover = function () {
+
+console.log('pasaste el mouse');
+}
+
+/*navBar.addEventListener('mouseover', function (){
+
+   console.log('pasaste el mouse');
+
+});*/
+
+let elImage = document.querySelector('.image');
+
+console.log(elImage);
+
+let navItem = document.querySelector('.nav-item');
+
+console.log(navItem);
+
+let elContainer = document.querySelector('.container');
+
+console.log(elContainer);
+
+let formGroup = document.querySelector('.form-group');
+
+console.log(formGroup);
+
